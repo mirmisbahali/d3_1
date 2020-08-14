0f1b12d0d019e36314a2bb375af798b7fddd933a
Mir Misbah Ali <mirmisbahali@gmail.com>
2020-08-15 04:14:22 +0530

    Completed Making Bar Graph

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const scaleBand = d3.scaleBand;
 const max = d3.max;
 const axisLeft = d3.axisLeft;
 const axisBottom = d3.axisBottom;
+const format = d3.format;
+
 
 const svg = select('svg');
 
@@ -14,7 +16,7 @@ const height = +svg.attr('height');
 const render = data => {
     const xValue = d => d.population;
     const yValue = d => d.country;
-    const margin = {top: 20, right: 40, bottom: 20, left: 100}
+    const margin = {top: 50, right: 40, bottom: 70, left: 200}
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
 
@@ -26,15 +28,37 @@ const render = data => {
                     .domain(data.map(d => yValue(d)))
                     .range([0, innerHeight])
                     .padding(0.1);
-                    
+
 
     const g = svg.append('g')
                 .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
+    
+    const xAxisTickFormat = number => format('.3s')(number).replace('G', "B")
 
-    g.append('g').call(axisLeft(yScale));
-    g.append('g').call(axisBottom(xScale))
-        .attr('transform', `translate(0, ${innerHeight})`);
+    const xAxis = axisBottom(xScale)
+                .tickFormat(xAxisTickFormat)
+                .tickSize(-innerHeight);
+
+
+    g.append('g')
+    .call(axisLeft(yScale))
+    .selectAll('.domain, .tick line')
+    .remove();
+
+
+    const xAxisG = g.append('g').call(xAxis)
+        .attr('transform', `translate(0, ${innerHeight})`)
+        
+    xAxisG.select('.domain')
+        .remove();;
+    
+    xAxisG.append('text')
+        .attr('class', 'axis-lable')
+        .attr('y', 60)
+        .attr('x', innerWidth / 2)
+        .attr('fill', 'black')
+        .text('Population')
 
     g.selectAll('react')
         .data(data)
@@ -42,7 +66,12 @@ const render = data => {
         .append('rect')
         .attr('y', d => yScale(yValue(d)))
         .attr('width', d => xScale(xValue(d)))
-        .attr('height', yScale.bandwidth())
+        .attr('height', yScale.bandwidth());
+
+    g.append('text')
+        .attr('class', 'title')
+        .attr('y', -10)
+        .text('Top 10 Most Popular Countries');
 }
 
 csv('data.csv').then(data => {
